3ac3f53c80dc129ccbfb558574b697da60bfbb51
Colton Fox <46362482+coltfox@users.noreply.github.com>
2024-09-01 20:37:22 -0400

    Custom lowpass filter using fourier transforms

--- spike-filtering.py
+++ spike-filtering.py
@@ -3,52 +3,21 @@ import numpy as np
 from if_neuron import IF_neuron
 from numpy.typing import ArrayLike
 from nengo.synapses import Lowpass
-from scipy import signal
 
 
-def generate_spikes(n_steps: int, freq: int = 0.1, amplitude: float = 1):
-    """
-    Generate spikes give an frequency
+def lowpass_filter(x: ArrayLike, sample_freq: int = 1000, cutoff_freq: int = 100):
+    # FFT of the signal
+    freq_domain = np.fft.fft(x)
 
-    Args:
-        n_steps (int): Number of time steps
-        freq (int): Frequency of spikes
-        amplitude (float): Amplitude of spikes
-    """
-    spikes = np.zeros((n_steps,))
-    # Time steps between each spike
-    ts = freq * n_steps
+    # Create a low-pass filter
+    freqs = np.fft.fftfreq(len(x), d=1 / sample_freq)
+    low_pass_filter = np.abs(freqs) < cutoff_freq
 
-    for t in range(n_steps):
-        if t % ts != 0:
-            continue
+    # Apply the filter in the frequency domain
+    filtered_freq_domain = freq_domain * low_pass_filter
 
-        spikes[t] = amplitude
-
-    return spikes
-
-# f(t) = (1 / tau) * exp(-t / tau)
-
-
-def filter_spikes(spikes: ArrayLike):
-    # Source: https://lonewritings.github.io/2018/05/26/signal_filtering.html
-    # sampling rate defines number of observations in one second.
-    sampling_rate = 4000
-
-    # Suppose the cut-off frequency is 100
-    fc = 1000
-    # Design of digital filter requires cut-off frequency to be normalised by sampling_rate/2
-    w = fc / (sampling_rate / 2)
-    num, den = signal.butter(5, w, 'low', analog=False)
-    return signal.filtfilt(num, den, spikes)
-    # return signal.lfilter(num, den, spikes)
-
-
-def lowpass_filter(x: ArrayLike, t: ArrayLike, tau: float = 1e-3):
-    # tau = max(1 - synapse, )
-    # Source: https://github.com/nengo/nengo/blob/30f711c26479a94e486ab1862a1400dce5b3ffa0/nengo/synapses.py#L436
-    filt = (1 / tau) * np.exp(-t / tau)
-    return np.convolve(x, filt, mode='same')
+    # Inverse FFT to get back to the time domain
+    return np.fft.ifft(filtered_freq_domain)
 
 
 DURATION = 10
@@ -58,12 +27,11 @@ timesteps = np.arange(0, DURATION, DT)
 
 inp = np.sin(timesteps)
 spikes = IF_neuron.spike_input(inp)
-# Test nengo filters for comparions
+# Test nengo filters for comparisons
 tau = 1e-3
 syn = Lowpass(tau)
 filtered_nengo = syn.filt(spikes)
-# filtered = lowpass_filter(spikes, timesteps, tau=1 - tau)
-filtered = filter_spikes(spikes)
+filtered = lowpass_filter(spikes)
 
 plt.figure()
 plt.xlabel('t')
